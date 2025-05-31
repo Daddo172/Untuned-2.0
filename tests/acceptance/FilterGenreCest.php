88ec31a402d49bd8458a72ff0c7f360fea0b7e07
DavideScolam <scolamierod@gmail.com>
2025-05-31 13:15:54 +0200

    .

--- tests/acceptance/FilterGenreCest.php
+++ tests/acceptance/FilterGenreCest.php
@@ -3,11 +3,11 @@ class FilterGenreCest
 {
     public function filterPopGenre(AcceptanceTester $I)
     {
-        $I->amOnPage('/Untuned-2.0-main/index.php'); // Assicurati che il percorso sia corretto
-        $I->see('Filter'); // Verifica che il filtro sia visibile
-        $I->selectOption('select[name=genere]', 'Pop'); // Selettore "select" con name="genere"
-        $I->click('Filter'); // Il nome esatto del bottone deve essere "Filter" o simile
-        $I->see('Pop'); // Verifica che i post Pop siano visibili
-        $I->dontSee('Rock'); // Controlla che altri generi siano nascosti
+        $I->amOnPage('/Untuned-2.0/index.php');
+        $I->see('Genere'); // Etichetta visibile del filtro
+        $I->selectOption('select[name=inputgenerefiltro]', 'Pop');
+        $I->click('Applica'); // Etichetta esatta del bottone
+        $I->see('Pop'); // Verifica che il genere Pop sia presente nei risultati
+        $I->dontSee('Rock'); // Verifica che altri generi non siano presenti
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'be5de3dab355f65c76e4791a638ba178aa4cd344',
+        'reference' => 'a2123bb22a74858bc64563b86499cc7f078157d8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'be5de3dab355f65c76e4791a638ba178aa4cd344',
+            'reference' => 'a2123bb22a74858bc64563b86499cc7f078157d8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
